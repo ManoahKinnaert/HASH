688e39631ca981a548558cf95854bbacdba88fd2
Manoah Kinnaert <manoah.k@icloud.com>
2026-04-22 14:22:42 +0200

    Updated test to force linear probing

--- src/test/java/core/testHashTableProbed.java
+++ src/test/java/core/testHashTableProbed.java
@@ -6,16 +6,10 @@ This is not a unit test. Just a quick test to see if HashTableProbed works...
 public class testHashTableProbed {
     public static void main(String[] args) {
         HashTableProbed<String, Integer> table = new HashTableProbed<String, Integer>();
-		table.put("Apple", 15);
+		table.put("Aa", 1);
 		System.out.println("Hits " + table.getHits());
 		System.out.println("Misses: " + table.getMisses());
-		table.put("Banana", 2);
-		System.out.println("Hits " + table.getHits());
-		System.out.println("Misses: " + table.getMisses());
-		table.put("Apple", 12);
-		System.out.println("Hits " + table.getHits());
-		System.out.println("Misses: " + table.getMisses());
-		table.get("Apple");
+		table.put("BB", 2);
 		System.out.println("Hits " + table.getHits());
 		System.out.println("Misses: " + table.getMisses());
     }
